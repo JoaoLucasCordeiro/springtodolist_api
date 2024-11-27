c150eb551e26beba31f94487d8dacee8bbbc8d4b
JoaoLucasCordeiro <joaolucascttprofissional@gmail.com>
2024-11-27 15:07:22 -0300

    feat: add task repository

---
+++ src/main/java/com/lucas/todolist/repository/TaskRepository.java
@@ -0,0 +1,7 @@
+package com.lucas.todolist.repository;
+
+import com.lucas.todolist.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TaskRepository extends JpaRepository<Task, Long> {
+}
