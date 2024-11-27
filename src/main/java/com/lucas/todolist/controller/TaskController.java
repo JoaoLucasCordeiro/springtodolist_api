df11e0b498f2ba37263525d380d424a6332625b2
JoaoLucasCordeiro <joaolucascttprofissional@gmail.com>
2024-11-27 15:06:52 -0300

    feat: add task controller

---
+++ src/main/java/com/lucas/todolist/controller/TaskController.java
@@ -0,0 +1,58 @@
+package com.lucas.todolist.controller;
+
+import com.lucas.todolist.model.Task;
+import com.lucas.todolist.repository.TaskRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/task")  // Agora todos os endpoints terão "/task" como base
+public class TaskController {
+    private final TaskRepository taskRepository;
+
+    public TaskController(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    @PostMapping
+    public Task createTask(@RequestBody Task task) {
+        return taskRepository.save(task);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Task> getTaskById(@PathVariable long id) {
+        return taskRepository.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Task> updateTask(@PathVariable Long id, @RequestBody Task taskDetails) {
+        return taskRepository.findById(id)
+                .map(task -> {
+                    task.setTitle(taskDetails.getTitle());
+                    task.setDescription(taskDetails.getDescription());
+                    task.setCompleted(taskDetails.isCompleted());
+                    return ResponseEntity.ok(taskRepository.save(task));
+                })
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTask(@PathVariable Long id) {
+        return taskRepository.findById(id)
+                .map(task -> {
+                    taskRepository.delete(task);
+                    return ResponseEntity.noContent().<Void>build();
+                })
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+}
+
